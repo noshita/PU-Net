5e3058398893eb113c94acc0d4d825288195737b
Koji Noshita <noshita@morphometrics.jp>
2018-08-15 22:06:54 +0900

    Py3

--- code/tf_ops/emd/tf_auctionmatch_compile.sh
+++ code/tf_ops/emd/tf_auctionmatch_compile.sh
@@ -1,4 +1,9 @@
+#/bin/bash
+
+TF_INC=$1
+TF_LIB=$2
+
 echo 'nvcc'
-/usr/local/cuda-8.0/bin/nvcc tf_auctionmatch_g.cu -o tf_auctionmatch_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_30 
+/usr/local/cuda/bin/nvcc tf_auctionmatch_g.cu -o tf_auctionmatch_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_30 
 echo 'g++'
-g++ -std=c++11 tf_auctionmatch.cpp tf_auctionmatch_g.cu.o -o tf_auctionmatch_so.so -shared -fPIC -D_GLIBCXX_USE_CXX11_ABI=0 -I /home/lqyu/software/anaconda2/lib/python2.7/site-packages/tensorflow/include  -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2
+g++ -std=c++11 tf_auctionmatch.cpp tf_auctionmatch_g.cu.o -o tf_auctionmatch_so.so -shared -fPIC -D_GLIBCXX_USE_CXX11_ABI=0 -I $TF_INC/external/nsync/public -I $TF_INC  -I /usr/local/cuda/include -lcudart -L /usr/local/cuda/lib64/  -L$TF_LIB -ltensorflow_framework -O2
--- code/tf_ops/interpolation/tf_interpolate_compile.sh
+++ code/tf_ops/interpolation/tf_interpolate_compile.sh
@@ -1,1 +1,6 @@
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /home/lqyu/software/anaconda2/lib/python2.7/site-packages/tensorflow/include  -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+#/bin/bash
+
+TF_INC=$1
+TF_LIB=$2
+
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I $TF_INC/external/nsync/public -I $TF_INC  -I /usr/local/cuda/include -lcudart -L /usr/local/cuda/lib64/ -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
